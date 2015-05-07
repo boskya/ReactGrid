619ab1a6c7b2274716c0d05f9d4f687651945349
boskya <bosky.atlani@changepoint.com>
2015-05-07 14:37:33 -0700

    adding client settings

--- src/app/Grid.js
+++ src/app/Grid.js
@@ -2,6 +2,7 @@
 var React = require('react');
 var Reflux = require('reflux');
 var TasksStore = require('./stores/TaskStore');
+var ClientSettingsStore = require('./stores/ClientSettingsStore';)
 
 var Grid = React.createClass({
     mixins: [Reflux.connect(TasksStore)],
