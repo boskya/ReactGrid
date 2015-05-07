9eb522263cc60843878279e43b7053c12e668c0b
boskya <bosky.atlani@changepoint.com>
2015-05-07 15:26:36 -0700

    client settings used to control columns

--- src/app/Grid.js
+++ src/app/Grid.js
@@ -4,17 +4,24 @@ var Reflux = require('reflux');
 var TasksStore = require('./stores/TaskStore');
 var ClientSettingsStore = require('./stores/ClientSettingsStore');
 var Row = require('./Row.js');
+var _ = require('lodash');
+
 
 var Grid = React.createClass({
-    mixins: [Reflux.connect(TasksStore)],
+    mixins: [Reflux.connect(TasksStore), Reflux.connect(ClientSettingsStore)],
     render() {
         var tasks = this.state.tasks.tasks;
         var tasksSchema = this.state.tasks.schema;
+        var taskGridSettings = this.state.clientSettings.taskGridSettings;
+        var taskGridSettingsSorted = _.sortBy(taskGridSettings, "Order");
+        var displaySchema = taskGridSettingsSorted.map(function(taskFieldSetting){
+            return _.find(tasksSchema, { 'Name': taskFieldSetting.Name })
+        });
         return (
             <table id="grid" className="gridContainer">
                 <tr className="headerRow">
                 {
-                  this.state.tasks.schema.map(function(taskField){
+                  displaySchema.map(function(taskField){
                     return (
                       <th>{taskField.DisplayName}</th>
                     );
@@ -23,7 +30,7 @@ var Grid = React.createClass({
                 </tr>
                 {
                     this.state.tasks.tasks.map(function (task) {
-                        return <Row schema={tasksSchema} task={task}/>;
+                        return <Row schema={displaySchema} task={task}/>;
                     })
                 }
             </table>
--- src/app/stores/ClientSettingsStore.js
+++ src/app/stores/ClientSettingsStore.js
@@ -1,66 +1,67 @@
 var Reflux = require('reflux');
 var React = require('react');
+var _ = require('lodash');
 
 
 var clientSettings = {
-  taskGridSettings : {
-    col: {
+  taskGridSettings : [
+    {
         "Name": "Number",
         "Order": 1
     },
-    col: {
+    {
         "Name": "Title",
         "Order": 2
     },
-    col: {
+    {
         "Name": "ActualFinish",
         "Order": 3
     },
-    col: {
+    {
         "Name": "ActualStart",
         "Order": 4
     },
-    col: {
+    {
         "Name": "Assignments",
         "Order": 5
     },
-    col: {
+    {
         "Name": "DurationMinutes",
         "Order": 6
     },
-    col: {
+    {
         "Name": "OnHold",
         "Order": 7
     },
-    col: {
+    {
         "Name": "OutlineNumber",
         "Order": 8
     },
-    col: {
+    {
         "Name": "PercentComplete",
         "Order": 9
     },
-    col:{
+    {
         "Name": "PlannedFinish",
         "Order": 10
     },
-    col:{
+    {
         "Name": "PlannedStart",
         "Order": 11
     },
-    col:{
+    {
         "Name": "Priority",
         "Order": 12
     },
-    col:{
+    {
         "Name": "Status",
         "Order": 13
     },
-    col:{
+    {
         "Name": "TaskType",
         "Order": 14
     }
-  }
+  ]
 };
 
 var ClientSettingsStore = Reflux.createStore({
