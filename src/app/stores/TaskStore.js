1b65d04a91be365a17ba11ccd25fcab44c7bdbd4
James Trinklein <james.trinklein@changepoint.com>
2015-05-08 10:35:57 -0700

    better dates

--- src/app/stores/TaskStore.js
+++ src/app/stores/TaskStore.js
@@ -2,10 +2,18 @@ var Reflux = require('reflux');
 var React = require('react');
 var _ = require('lodash');
 var tasks = require('./tasksData');
+var moment = require('moment');
 
 var TasksStore = Reflux.createStore({
+    
     getInitialState() {
-        
+        tasks.tasks = tasks.tasks.map(function (t) {
+            if(t.PlannedFinish) { t.PlannedFinish = moment(t.PlannedFinish).format('YYYY-MM-DD'); }
+            if(t.PlannedStart) { t.PlannedStart = moment(t.PlannedStart).format('YYYY-MM-DD'); }
+            if(t.ActualStart) { t.ActualStart = moment(t.ActualStart).format('YYYY-MM-DD'); }
+            if(t.ActualFinish) { t.ActualFinish = moment(t.ActualFinish).format('YYYY-MM-DD'); }
+            return t;
+        });
         return {tasks};
     }
 });
