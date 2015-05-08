a7140c44655a5344089323bea5f340fdcd1f114a
Chris Bobo <chrisbobo@gmail.com>
2015-05-08 13:14:53 -0700

    Checkin generated data

--- src/app/Grid.js
+++ src/app/Grid.js
@@ -10,6 +10,7 @@ var _ = require('lodash');
 var Grid = React.createClass({
     mixins: [Reflux.connect(TasksStore), Reflux.connect(ClientSettingsStore)],
     render() {
+        debugger;
         var tasks = this.state.tasks.tasks;
         var tasksSchema = this.state.tasks.schema;
         var taskGridSettings = this.state.clientSettings.taskGridSettings;
